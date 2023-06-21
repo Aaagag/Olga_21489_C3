121310752c670b1c7fe0851a29546061a99eda37
Aaagag <137276832+Aaagag@users.noreply.github.com>
2023-06-21 21:08:36 +0200

    Add files via upload

--- Zaliczenie/src/main/java/Paczka/Account.java
+++ Zaliczenie/src/main/java/Paczka/Account.java
@@ -6,48 +6,38 @@ public class Account extends Customer {
     private int id;
     private Customer customer;
     private double balance = 0.0;
-
     public Account(int id, Customer customer, double balance) {
         this.id = id;
         this.customer = customer;
         this.balance = balance;
     }
-
     public Account(int id, Customer customer) {
         this.id = id;
         this.customer = customer;
     }
-
     public int getId() {
         return this.id;
     }
-
     public Customer getCustomer() {
         return this.customer;
     }
-
     public double getBalance() {
         return this.balance;
     }
-
     public void setBalance(double balance) {
         this.balance = balance;
     }
-
     public String toString() {
         String var10000 = this.customer.toString();
         return var10000 = stanKonta() + this.balance;
     }
-
     public String getCustomerName() {
         return this.customer.getName();
     }
-
     public Account deposit(double amount) {
         this.balance += amount;
         return null;
     }
-
     public Account withdraw(double amount) {
         if (this.balance >= amount) {
             this.balance -= amount;
@@ -56,7 +46,6 @@ public class Account extends Customer {
         } else {
             System.out.println(odmowa());
         }
-
         return null;
     }
 }
